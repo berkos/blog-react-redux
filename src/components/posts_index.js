b954d5a64bc19c1e3db61089fc37c16bd9236c9d
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-10-29 13:44:57 +0000

    Render posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from "../actions/index";
@@ -7,13 +8,31 @@ class PostIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    //console.log(this.props.posts);
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect(null, {fetchPosts: fetchPosts})(PostIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, {fetchPosts: fetchPosts})(PostIndex);
