16d60747d5c9acd53c69092f3c9f8211a20c73e1
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-11-01 12:52:31 +0000

    Create some forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,68 @@
 import React, { Component } from 'react';
+import { Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+      return (
+        <div className="form-group">
+          <label>Title:</label>
+          <input
+            className="form-control"
+            //instead of
+            // onChange={field.input.onChange}
+            // onFocus={field.input.onFocus}
+            // this wires up everything
+            type="text"
+            {...field.input}
+          />
+        </div>
+      );
+  }
+
+  renderTagsField(field) {
+      return (
+        <div className="form-group">
+          <label>{field.label}:</label>
+          <input
+            className="form-control"
+            //instead of
+            // onChange={field.input.onChange}
+            // onFocus={field.input.onFocus}
+            // this wires up everything
+            type="text"
+            {...field.input}
+          />
+        </div>
+      );
+  }
+
   render() {
     return (
       <div>
-        PostsNew!
+        <form>
+          <Field
+            label="Title"
+            name="title"
+            component={this.renderField}
+          />
+
+          <Field
+            label="Tags"
+            name="tags"
+            component={this.renderField}
+          />
+
+          <Field
+            label="Content"
+            name="content"
+            component={this.renderField}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
