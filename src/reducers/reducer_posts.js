c19b4806fc891bb9cfa003daac0fecbefc0013c6
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-11-02 15:51:58 +0000

    Set up reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
-import { FETCH_POSTS}  from "../actions/index";
+import { FETCH_POSTS, FETCH_POST}  from "../actions/index";
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      // ES5
+      //const post = action.payload.data;
+      //const newState = {...state};
+      //newState[post.id] = post;
+      //return newState;
+      //return { ...state, post};
+
+      // ES6
+      return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       //console.log(action.payload.data); // [post1, post2]
       // { 4: post }
