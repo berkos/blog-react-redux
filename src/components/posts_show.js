1e5c2b555cb24c554b331bf0899247d1e7715793
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-11-02 18:35:51 +0000

    Link to posts in index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPost} from "../actions/index";
+import { Link } from 'react-router-dom';
+
 
 
 class PostsShow extends Component {
@@ -18,6 +20,7 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back to Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
